4d7f2f00986f6b9cf65e6328376d1c54d0d6acd9
Adrien <apointreau@gmail.com>
2022-04-24 15:13:52 +0200

    Formatted and added logging

--- bot.py
+++ bot.py
@@ -7,34 +7,35 @@ from tickets_finder import *
 def start(update, context):
     """Send a message when the command /start is issued."""
     chat_id = update.message.chat_id
-    update.message.reply_text('Hi! I am looking for '+
-    'tickets from '+ departure_station_name +' to '+
-    arrival_station_name + ' on date '+ day_of_departure + ".\n\n"+
-    'Every few minutes, I do an online scan : '+
-    'if I find one, I notify you.\n\nYou can '+
-    'still do /tickets to have an immediate result ! \U0001f603')
-    context.job_queue.run_repeating(loop_tickets,interval= look_interval, #in seconds
-        first=0, context=chat_id, job_kwargs=None)
+    update.message.reply_text('Hi! I am looking for ' +
+                              'tickets from ' + departure_station_name + ' to ' +
+                              arrival_station_name + ' on date ' + day_of_departure + ".\n\n" +
+                              'Every few minutes, I do an online scan : ' +
+                              'if I find one, I notify you.\n\nYou can ' +
+                              'still do /tickets to have an immediate result ! \U0001f603')
+    context.job_queue.run_repeating(loop_tickets, interval=look_interval,  # in seconds
+                                    first=0, context=chat_id, job_kwargs=None)
+
 
 def loop_tickets(context):
     job = context.job
     nb_tickets, best_price = look_for_tickets()
-    if nb_tickets >0:
-        context.bot.send_message(job.context, text='Found '+ 
-        str(nb_tickets) + ' tickets starting at '+
-        str(best_price)+ '€ with carte avantage jeune ! \U0001f603')
+    if nb_tickets > 0:
+        context.bot.send_message(job.context, text='Found ' +
+                                 str(nb_tickets) + ' tickets starting at ' +
+                                 str(best_price) + '€ with carte avantage jeune ! \U0001f603')
+
 
 def tickets(update, context):
-    
-    nb_tickets, best_price = look_for_tickets()
-    if nb_tickets >0:
-        update.message.reply_text('Found '+ 
-        str(nb_tickets) + ' tickets starting at '+
-        str(best_price)+ '€ with carte avantage jeune !')
-    else :
-        update.message.reply_text(''+
-        'No tickets found \U0001f622')
 
+    nb_tickets, best_price = look_for_tickets()
+    if nb_tickets > 0:
+        update.message.reply_text('\U0001f3ab Found ' +
+                                  str(nb_tickets) + ' tickets starting at ' +
+                                  str(best_price) + '€ with carte avantage jeune on date ' + day_of_departure + ' ! \U0001f603')
+    else:
+        update.message.reply_text('\U0000274c ' +
+                                  'No tickets found on date ' + day_of_departure + '. \U0001f622')
 
 
 def main():
--- tickets_finder.py
+++ tickets_finder.py
@@ -2,82 +2,81 @@ import requests
 import json
 import numpy as np
 import datetime
-from environment import * ##All the variable such as cards,headers...
+from environment import *  # All the variable such as cards,headers...
 
-           
+data = {
+    "local_currency": "EUR",
+    "search":
+    {"arrival_station_id": arrival_station_id,
+     "departure_date": departure_date,
+     "departure_station_id": departure_station_id,
+     "systems": ["sncf", "db", "idtgv", "ouigo",
+                         "trenitalia", "ntv", "hkx", "renfe", "cff",
+                         "benerail", "ocebo", "westbahn", "leoexpress",
+                         "locomore", "busbud", "flixbus", "distribusion",
+                         "cityairporttrain", "obb", "timetable"],
+     "passengers": [
+         {"id": "6ed7911c-ed2c-4745-80fd-64e173bb9328",
+          "age": passenger1_age,
+          "birthdate": passenger1_birthdate,
+          "cards": cards,  # [] for none
+          "label": "6ed7911c-ed2c-4745-80fd-64e173bb9328"
+          }
+     ]
+     }
+}
 
 
-def look_for_tickets() :
+def look_for_tickets():
 
     prices = []
-    
-    data = {
-        "local_currency": "EUR", 
-        "search": 
-            {"arrival_station_id": arrival_station_id,
-            "departure_date": departure_date, 
-            "departure_station_id": departure_station_id,
-            "systems": ["sncf", "db", "idtgv", "ouigo", 
-                "trenitalia", "ntv", "hkx", "renfe", "cff", 
-                "benerail", "ocebo", "westbahn", "leoexpress",
-                "locomore", "busbud", "flixbus", "distribusion",
-                "cityairporttrain", "obb", "timetable"], 
-            "passengers": [
-                {"id": "6ed7911c-ed2c-4745-80fd-64e173bb9328",
-                "age": passenger1_age,
-                "birthdate": passenger1_birthdate,
-                "cards": cards, # [] for none
-                "label": "6ed7911c-ed2c-4745-80fd-64e173bb9328"
-                }
-            ]
-        }
-    }
 
     post_data = json.dumps(data)
-    ret =requests.post(url="https://www.trainline.eu/api/v5_1/search", 
-        data=post_data, headers=headers)
-    #print(ret.json())
+    ret = requests.post(url="https://www.trainline.eu/api/v5_1/search",
+                        data=post_data, headers=headers)
+    # print(ret.json())
     nb_found_trip = 0
     nb_possible_trips = len(ret.json()["folders"])
-    with open('json_data.txt', 'w') as outfile:
-        for i in range(nb_possible_trips):
-            
-            if (ret.json()["folders"][i]["is_sellable"]
+
+    for i in range(nb_possible_trips):
+
+        if (ret.json()["folders"][i]["is_sellable"]
             and not(ret.json()["folders"][i]["arrival_different_from_requested"])
-            and limit_departure_date > ret.json()["folders"][i]["departure_date"]):
+                and limit_departure_date > ret.json()["folders"][i]["departure_date"]):
 
-                nb_found_trip +=1
-                prices.append(ret.json()["folders"][i]["cents"])
-                outfile.write(str(ret.json()["folders"][i]))
-                outfile.write("\n")
+            nb_found_trip += 1
+            prices.append(ret.json()["folders"][i]["cents"])
 
-        data["search"]["arrival_station_id"] = second_arrival_station_id
-        post_data = json.dumps(data)
-        ret =requests.post(url="https://www.trainline.eu/api/v5_1/search",
-        data=post_data, headers=headers)
+    #---Nouvelle recherche pour les ouigo (Massy TGV)---#
 
-        ### Nouvelle recherche pour les ouigo (Massy TGV)
+    data["search"]["arrival_station_id"] = second_arrival_station_id
+    post_data = json.dumps(data)
+    ret = requests.post(url="https://www.trainline.eu/api/v5_1/search",
+                            data=post_data, headers=headers)
 
-        nb_possible_trips += len(ret.json()["folders"])
-        for i in range(len(ret.json()["folders"])):
-            
-            if (ret.json()["folders"][i]["is_sellable"]
-            and not(ret.json()["folders"][i]["arrival_different_from_requested"])
-            and limit_departure_date > ret.json()["folders"][i]["departure_date"]):
+    nb_possible_trips += len(ret.json()["folders"])
+    for i in range(len(ret.json()["folders"])):
 
-                nb_found_trip +=1
-                prices.append(ret.json()["folders"][i]["cents"])
-                outfile.write(str(ret.json()["folders"][i]))
-                outfile.write("\n")
-    
+        if (ret.json()["folders"][i]["is_sellable"]
+                and not(ret.json()["folders"][i]["arrival_different_from_requested"])
+                and limit_departure_date > ret.json()["folders"][i]["departure_date"]):
+
+            nb_found_trip += 1
+            prices.append(ret.json()["folders"][i]["cents"])
+
     e = datetime.datetime.now()
-
-    if nb_found_trip >0:
-        best_price = np.min(prices)/100
-        print("Found ", nb_found_trip, " trips starting at ", best_price, "€ ! "+
-        '%s/%s/%s %s:%s' % (e.day, e.month, e.year, e.hour, e.minute))
-        return nb_found_trip, best_price
-    else:
-        print("No tickets but ", nb_possible_trips, " booked ones at "+
-        '%s/%s/%s %s:%s' % (e.day, e.month, e.year, e.hour, e.minute))
-        return 0, -1
+    with open('log.txt', 'a') as outfile:
+        if nb_found_trip > 0:
+            best_price = np.min(prices)/100
+            found_str = "Found " + str(nb_found_trip) + " trips starting at " + str(
+                best_price) + "€ ! at " + '%s/%s/%s %s:%s' % (e.day, e.month, e.year, e.hour, e.minute)
+            print(found_str)
+            outfile.write(found_str+'\n')
+            return nb_found_trip, best_price
+        else:
+            not_found_str = "No tickets but " + \
+                str(nb_possible_trips) + " booked ones at " + \
+                '%s/%s/%s %s:%s' % (e.day, e.month, e.year, e.hour, e.minute)
+            print(not_found_str)
+            outfile.write(not_found_str+'\n')
+            return 0, -1
